42886b7872c5139de6e86791d56db00b448e652f
sjiang88 <30816926+sjiang88@users.noreply.github.com>
2018-01-02 22:08:47 -0800

    Site responsivity fix - Mobile-friendly main page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-// Make it rain!
+// defines an array
+
+var items = [
+[1, 'img/stroke_call_colcorr-responsive.png', 'img/stroke_call_colcorr.png'],
+[2, 'img/mom-square-responsive.png', 'img/mom-rect.png'],
+[3, 'img/medx-wall-responsive.png', 'img/soundings-wall-2.png'],
+[4, 'img/peaches-square-responsive.png', 'img/peaches-square.png'],
+[5, 'img/360-vr-cam-responsive.png', 'img/vr-cam.png'],
+[6, 'img/360-collab-responsive.png', 'img/360vr-collab.png'],
+];
+
+function resize(projNumber, newSource, origSrc){
+
+	var x = '.proj-' + projNumber + ' img';
+
+	if ($(window).width() < 600) { 
+		console.log('passed check');
+		$(x).attr('src', newSource);
+	} 
+	else {
+		$(x).attr('src', origSrc);
+	}
+}
+
+function resizeAll() {
+	for (i = 0; i < items.length; i++) {
+		resize(items[i][0],items[i][1],items[i][2]);
+	}
+}
+
+$(window).on('load', resizeAll);
+$(window).on('resize', resizeAll);
